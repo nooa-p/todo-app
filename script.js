facbe25ee8302100bfd01f3392dc630cb1770440
Nooa Peltokangas <nooa.peltokangas@protonmail.com>
2023-10-25 11:59:39 +0300

    added background image to header, switches on mode toggle

--- script.js
+++ script.js
@@ -1,24 +1,27 @@
 const btn = document.querySelector('.btn-toggle');
+const header = document.querySelector('header');
 const prefer = window.matchMedia('(prefers-color-scheme: dark)');
-
-console.log(document.querySelector('.btn-toggle'));
 
 const current = localStorage.getItem('theme');
 if (current === 'dark') {
     document.body.classList.toggle('dark');
+    header.classList.toggle('dark');
 } else if (current === 'light') {
     document.body.classList.toggle('light');
+    header.classList.toggle('light');
 }
 
 btn.addEventListener("click", function() {
     if (prefer.matches) { 
         document.body.classList.toggle('light');
+        header.classList.toggle('light');
         var theme = document.body.classList.contains('light')
             ? 'light'
             : 'dark';
         
     } else { 
         document.body.classList.toggle('dark');
+        header.classList.toggle('light');
         var theme = document.body.classList.contains('dark')
             ? 'dark'
             : 'light';
