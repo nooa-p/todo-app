7bab457843731fee5ee52bedd7730e6d9c8fedfa
Nooa Peltokangas <nooa.peltokangas@protonmail.com>
2023-10-27 09:47:36 +0300

    close button working, also on new todos

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ const header = document.querySelector('header');
 const prefer = window.matchMedia('(prefers-color-scheme: dark)');
 var list = document.querySelector('.list');
 
+
+// dark & light theme saved to local storage
 const current = localStorage.getItem('theme');
 if (current === 'dark') {
     document.body.classList.toggle('dark');
@@ -12,6 +14,7 @@ if (current === 'dark') {
     header.classList.toggle('light');
 }
 
+// dark & light theme toggle
 btn.addEventListener("click", function() {
     if (prefer.matches) { 
         document.body.classList.toggle('light');
@@ -30,25 +33,44 @@ btn.addEventListener("click", function() {
     localStorage.setItem('theme', theme);
 });
 
+// checking and unchecking todos
 list.addEventListener('click', function(evt) {
     if (evt.target.tagName === 'LI') {
         evt.target.classList.toggle('checked');
     }
 }, false);
 
+// deleting a todo
+var close = document.getElementsByClassName('close');
+var i;
+for (i = 0; i < close.length; i++) {
+    close[i].onclick = function() {
+        var parent = this.parentElement;
+        parent.remove();
+    }
+}
+
+// adding a new todo
 document.getElementById('new-item').addEventListener('submit', function() {
     var li = document.createElement('li');
     var span = document.createElement('span');
     var input = document.getElementById('new-text').value;
-    var close = document.createElement('div');
-    close.className = 'close';
+    var cross = document.createElement('div');
+    cross.className = 'close';
 
     var t = document.createTextNode(input);
     span.appendChild(t);
     li.appendChild(span);
-    li.appendChild(close);
+    li.appendChild(cross);
     if (input != '') {
         list.appendChild(li);
     }
     document.getElementById('new-text').value = '';
+
+    for (i = 0; i < close.length; i++) {
+        close[i].onclick = function() {
+            var parent = this.parentElement;
+            parent.remove();
+        }
+    }
 });
